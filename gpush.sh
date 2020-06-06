c33576bd9bac03ad87b7090b36639ea476456ea6
gaurav porwal <gauravprwl14@gmail.com>
2020-06-06 08:04:52 +0530

    added the git push script

---
+++ gpush.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# First parameter will be the commit message
+readonly message=$1
+
+# Getting the current branch
+readonly currentBranch=$(git symbolic-ref --short -q HEAD)
+
+echo "currentBranch => $currentBranch"
+
+if [ ! -z "$message" ]
+then
+git add .
+git commit -m "$message"
+git push origin $currentBranch
+
+echo "Current changes is pushed to $currentBranch"
+else
+echo "Please provide commit message"
+fi
